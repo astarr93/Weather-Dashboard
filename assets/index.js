0fcfc94d8a51eae86c5e2253e3c3a3a91d689214
Andrew Starr <astarr93@outlook.com>
2020-08-04 08:09:56 -0400

    Refactored HTML Aside. Refactored, improved, and grouped searchHistory functions

--- assets/index.js
+++ assets/index.js
@@ -3,30 +3,28 @@ let userInput = "";
 let searchedItems = [];
 const searchBar = $('#search-bar')
 const searchButton = $('#search-button')
-const searchHistory = $('#search-history')
+const searchHistory = document.getElementById("search-history");
+const searchHistoryLabel = $('#search-history-label')
+const menu = $('#menu')
 
 // Start on DOM ready
 $(document).ready(function () {
+    displayHistory();
     searchClickEvent();
-    if (localStorage.userHistory) {
-        JSON.parse(localStorage.userHistory).forEach(function (record) {
-            searchedItems.push(record);
-        })
-        console.log(searchedItems);
-        displayHistory(searchedItems);
-    }
-})
+}
+)
 
 function searchClickEvent() {
     searchButton.on("click", function () {
         event.preventDefault();
         if (searchBar.val() !== '') {
             userInput = searchBar.val();
+            document.getElementById("search").reset()
+            // NEED TO VALIDATE INPUT BEFORE SUBMISSION
             startLookup(userInput); // Retreive data from OpenWeatherAPI
-            recordLookup(userInput);  // Record search and update history
         }
         else {
-            alert("You suck!");
+            alert("You suck!"); // PLAY WITH LABELS INSTEAD OF ALERTS
             return;
         }
     })
@@ -37,26 +35,49 @@ function searchClickEvent() {
 async function startLookup() {
     const accesstoken = "bf9f438089a6bdeedce9b06784b29a58";
     const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&units=imperial&appid=${accesstoken}`;
-    console.log(queryURL);
+    recordLookup(userInput); // Record search and update history
     await $.ajax({ url: queryURL, method: "GET" }).then(function (response) {
         data = JSON.stringify(response);
-        console.log(data);
+        // console.log(data);
     })
+}
+
+// Search History Functions
+
+function displayHistory() {
+    if (localStorage.userHistory) {
+        $('#search-history-label').removeClass("hidden");
+        searchedItems = JSON.parse(localStorage.userHistory);
+        searchedItems.forEach(function (item) {
+            $('#search-history').append($(`<button class="history-button">${item}</button>`));
+        })
+    }
 }
 
 function recordLookup() {
-    searchedItems.push(userInput);
-    localStorage.setItem("userHistory", JSON.stringify(searchedItems));
-
+    if (searchedItems.length > 10) {
+        searchedItems.pop();
+        searchedItems.unshift(userInput);
+        localStorage.setItem("userHistory", JSON.stringify(searchedItems));
+        manageHistory(userInput);
+    }
+    else {
+        searchedItems.unshift(userInput);
+        localStorage.setItem("userHistory", JSON.stringify(searchedItems));
+        manageHistory(userInput);
+    }
 }
 
-function displayHistory() {
-    searchHistory.append($('<p id="menu-header">History:</p>'));
-    // let city = JSON.parse(searchedItems);
-    searchedItems.forEach(function (item) {
-        searchHistory.append($(`<button class="history-button">${searchedItems.item}</button>`));
-    })
+function manageHistory() {
+    if (searchedItems.length > 10) {
+        $('#search-history').children().last().remove();
+        createButton(userInput);
+    }
+    else {
+        createButton(userInput);
+    }
 }
-// searchedItems.forEach(function (item) {
-//     searchHistory.append($(`<button class="history-button">${searchedItems}</button>`));
-// })
+
+function createButton() {
+    $('#search-history').prepend($(`<button class="history-button">${userInput}</button>`)); // Review Button Classes for CSS
+}
