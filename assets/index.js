dcf59befce8eb2eba9007ecec3f9f8657cac9e0b
Andrew Starr <astarr93@outlook.com>
2020-08-06 23:40:55 -0400

    refactored search functions to es6 and reduced lines. Added clear history button

--- assets/index.js
+++ assets/index.js
@@ -3,16 +3,21 @@ let userInput = "";
 let searchedItems = [];
 const searchBar = $('#search-bar')
 const searchButton = $('#search-button')
-const searchHistory = document.getElementById("search-history");
+const searchHistory = $("#search-history");
 const searchHistoryLabel = $('#search-history-label')
+const clearHistoryButton = $('#clear-history-button');
 const menu = $('#menu')
 
 // Start on DOM ready
 $(document).ready(function () {
-    displayHistory();
+    loadHistory();
     searchClickEvent();
+    clearClickEvent();
 }
 )
+
+
+// Click Events
 
 function searchClickEvent() {
     searchButton.on("click", function () {
@@ -30,12 +35,18 @@ function searchClickEvent() {
     })
 }
 
+function clearClickEvent() { // Clear Search History, Hide Search Label, Data Display
+    clearHistoryButton.on("click", function () {
+        localStorage.removeItem("userHistory");
+        location.reload();
+    })
+};
 
 // Pass in userInput to Open Weather API, response is weather data
 async function startLookup() {
     const accesstoken = "bf9f438089a6bdeedce9b06784b29a58";
     const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&units=imperial&appid=${accesstoken}`;
-    recordLookup(userInput); // Record search and update history
+    saveSearch(userInput); // Record search and update history
     await $.ajax({ url: queryURL, method: "GET" }).then(function (response) {
         data = JSON.stringify(response);
         // console.log(data);
@@ -44,9 +55,9 @@ async function startLookup() {
 
 // Search History Functions
 
-function displayHistory() {
+function loadHistory() { // Load local storage search results
     if (localStorage.userHistory) {
-        $('#search-history-label').removeClass("hidden");
+        $('.hidden').removeClass("hidden");
         searchedItems = JSON.parse(localStorage.userHistory);
         searchedItems.forEach(function (item) {
             $('#search-history').append($(`<button class="history-button">${item}</button>`));
@@ -54,30 +65,13 @@ function displayHistory() {
     }
 }
 
-function recordLookup() {
-    if (searchedItems.length > 10) {
-        searchedItems.pop();
-        searchedItems.unshift(userInput);
-        localStorage.setItem("userHistory", JSON.stringify(searchedItems));
-        manageHistory(userInput);
-    }
-    else {
-        searchedItems.unshift(userInput);
-        localStorage.setItem("userHistory", JSON.stringify(searchedItems));
-        manageHistory(userInput);
-    }
+function saveSearch() {
+    $(".hidden").removeClass("hidden");
+    const saveSearch = (searchedItems.length > 3) ? searchedItems.pop() && searchedItems.unshift(userInput) : searchedItems.unshift(userInput);
+    localStorage.setItem("userHistory", JSON.stringify(searchedItems));
+    const updateDisplay = (searchedItems.length > 3) ? searchHistory.children().last().remove() && createButton(userInput) : createButton(userInput);
 }
 
-function manageHistory() {
-    if (searchedItems.length > 10) {
-        $('#search-history').children().last().remove();
-        createButton(userInput);
-    }
-    else {
-        createButton(userInput);
-    }
-}
-
-function createButton() {
-    $('#search-history').prepend($(`<button class="history-button">${userInput}</button>`)); // Review Button Classes for CSS
+function createButton() { // Create button from input and append to history
+    searchHistory.prepend($(`<button class="history-button">${userInput}</button>`));
 }
