af1580f137b75a10b17320dbda21f759f0ddcf40
Andrew Starr <astarr93@outlook.com>
2020-08-16 16:22:53 -0400

    updated search functions and logging

--- assets/index.js
+++ assets/index.js
@@ -1,24 +1,27 @@
-// Global Variables
+// Search Menu Vars
 let userInput = "";
 let searchedItems = [];
-const searchBar = $('#search-bar')
-const searchButton = $('#search-button')
+const menu = $('#menu') // Side Bar
+const searchBar = $('#search-bar');
+const searchButton = $('#search-button');
 const searchHistory = $("#search-history");
-const searchHistoryLabel = $('#search-history-label')
+const searchHistoryLabel = $('#search-history-label');
 const clearHistoryButton = $('#clear-history-button');
-const menu = $('#menu')
+// Data Display Vars
+const weatherDisplay = $('#current-weather');
+const weatherLocation = $('#location');
 
-// Start on DOM ready
+
+
+// DOM Start
 $(document).ready(function () {
     loadHistory();
     searchClickEvent();
+    historyClickEvent();
     clearClickEvent();
-}
-)
-
+})
 
 // Click Events
-
 function searchClickEvent() {
     searchButton.on("click", function () {
         event.preventDefault();
@@ -26,7 +29,8 @@ function searchClickEvent() {
             userInput = searchBar.val();
             document.getElementById("search").reset()
             // NEED TO VALIDATE INPUT BEFORE SUBMISSION
-            startLookup(userInput); // Retreive data from OpenWeatherAPI
+            currentWeather(userInput); // Retreive data from OpenWeatherAPI
+            // currentForecast(userInput);
         }
         else {
             alert("You suck!"); // PLAY WITH LABELS INSTEAD OF ALERTS
@@ -35,43 +39,104 @@ function searchClickEvent() {
     })
 }
 
+function historyClickEvent() {
+    $('.history-button').on("click", function () {
+        event.preventDefault();
+        userInput = this.value;
+        currentWeather(userInput);
+        // currentForecast(userInput);
+    })
+}
+
 function clearClickEvent() { // Clear Search History, Hide Search Label, Data Display
     clearHistoryButton.on("click", function () {
         localStorage.removeItem("userHistory");
+        resetDisplay();
         location.reload();
-    })
-};
+    });
+}
 
-// Pass in userInput to Open Weather API, response is weather data
-async function startLookup() {
+function resetDisplay() { // Clears information in display
+    let display = document.getElementById('current-weather');
+    while (display.firstChild) {
+        display.removeChild(display.firstChild);
+    }
+}
+
+
+
+// Pass in userInput from Search/History to Open Weather API for current weather data response
+function currentWeather() {
     const accesstoken = "bf9f438089a6bdeedce9b06784b29a58";
     const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&units=imperial&appid=${accesstoken}`;
     saveSearch(userInput); // Record search and update history
-    await $.ajax({ url: queryURL, method: "GET" }).then(function (response) {
-        data = JSON.stringify(response);
-        // console.log(data);
+    // Clear data display
+    resetDisplay();
+    $.ajax({ url: queryURL, method: "GET" }).then(function (response) {
+        console.log(response);
+        // Grab and store currentWeather Data
+        let today = new Date();
+        let date = (today.getMonth() + 1) + '/' + (today.getDate()) + '/' + (today.getFullYear());
+        let city = response.name;
+        let icon = response.weather[0].icon;
+        let iconURL = `http://openweathermap.org/img/w/${icon}.png`;
+        let temperature = "Temperature: " + response.main.temp + " F";
+        let humidity = "Humidity: " + response.main.humidity + "%";
+        let windspeed = "Wind Speed: " + response.wind.speed + " MPH";
+        let coords = [response.coord.lon, response.coord.lat];
+        let uvindex = getUVIndex(coords);
+        console.log(uvindex);
+        // Display Current Weather Data
+        weatherDisplay.append($(`<h1>${city} ${date} <img src="${iconURL}"${icon}/></h1>`));
+        weatherDisplay.append($(`<h6>${temperature}</h6>`));
+        weatherDisplay.append($(`<h6>${humidity}</h6>`));
+        weatherDisplay.append($(`<h6>${windspeed}</h6>`));
+        weatherDisplay.append($(`<h6 id="uvindex">${uvindex}</h6>`));
+    })
+}
+
+
+function getUVIndex(coords) {
+    const accesstoken = "bf9f438089a6bdeedce9b06784b29a58";
+    const queryURL = `https://api.openweathermap.org/data/2.5/uvi?appid=${accesstoken}&lat=${coords[0]}&lon=${coords[1]}`;
+    $.ajax({ url: queryURL, method: "GET" }).then((response) => response.value);
+    console.log(response.value);
+}
+
+function currentForecast() {
+    const accesstoken = "bf9f438089a6bdeedce9b06784b29a58";
+    const queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${userInput}&units=imperial&appid=${accesstoken}`;
+    $.ajax({ url: queryURL, method: "GET" }).then(function (response) {
+        return;
+        // weatherDisplay.append($(`<h1>${response.name}`)
     })
 }
 
 // Search History Functions
-
 function loadHistory() { // Load local storage search results
     if (localStorage.userHistory) {
-        $('.hidden').removeClass("hidden");
+        $('#menu-header').removeClass("hidden");
+        clearHistoryButton.removeClass("hidden");
         searchedItems = JSON.parse(localStorage.userHistory);
-        searchedItems.forEach(function (item) {
-            $('#search-history').append($(`<button class="history-button">${item}</button>`));
+        searchedItems.forEach(function (userInput) {
+            searchHistory.append($(`<button type="submit" class="history-button" value="${userInput}">${userInput}</button>`));
         })
     }
 }
 
-function saveSearch() {
+function saveSearch() { // Save search event and reveal history and data display
     $(".hidden").removeClass("hidden");
-    const saveSearch = (searchedItems.length > 3) ? searchedItems.pop() && searchedItems.unshift(userInput) : searchedItems.unshift(userInput);
+    if (searchedItems.length < 5) searchedItems.unshift(userInput);
+    else {
+        searchedItems.pop();
+        searchedItems.unshift(userInput);
+        searchHistory.children().last().remove()
+    }
     localStorage.setItem("userHistory", JSON.stringify(searchedItems));
-    const updateDisplay = (searchedItems.length > 3) ? searchHistory.children().last().remove() && createButton(userInput) : createButton(userInput);
+    createButton(userInput);
 }
 
 function createButton() { // Create button from input and append to history
-    searchHistory.prepend($(`<button class="history-button">${userInput}</button>`));
+    searchHistory.prepend($(`<button type="submit" class="history-button" value="${userInput}">${userInput}</button>`));
 }
+
